29a08274cc33e438092d6881fc0722ecc51eb16c
Zahin_Abdullah <zahinabdullahrakin@gmail.com>
2024-08-21 09:00:49 +0600

    module type css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,10 +2,12 @@
 // import Footer from './lesson-1/Footer.jsx';
 // import Food from './lesson-1/Food.jsx';
 // import Card from './lesson-2/card.jsx';
+import Button from "./button/Button.jsx";
 
 function App() {
   return(
     <>
+      <Button/>
     </>
   );
 }
